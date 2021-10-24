2d3c24a8f84e13073d670512a061360e0c8822f7
Ashok-reddy340 <ashoklakki94.com>
2021-10-24 14:15:31 +0530

    add user and view user functions in component

--- BookStore-Angular/src/app/admin/users/users.component.ts
+++ BookStore-Angular/src/app/admin/users/users.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 
-import { Customer } from 'src/app/model/Customer';
+import { Component, OnInit } from '@angular/core';
 import { HttpClientService } from 'src/app/service/http-client.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer } from '../../model/Customer';
+
 
 @Component({
   selector: 'app-users',
@@ -10,18 +12,49 @@ import { HttpClientService } from 'src/app/service/http-client.service';
 })
 export class UsersComponent implements OnInit {
 
+  
   users: Array<Customer>;
+  selectedUser: Customer ;
+  action: string;
 
-  constructor(private httpClientService: HttpClientService) { }
+
+
+
+  constructor(private httpClientService: HttpClientService,private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.refreshData();
+  }
+  refreshData() {
     this.httpClientService.getUsers().subscribe(
       response => this.handleSuccessfulResponse(response),
+    );
+
+    this.activatedRoute.queryParams.subscribe(
+      (params) => {
+        this.action = params['action'];
+        const selectedUserId = params['id'];
+        if (selectedUserId) {
+          this.selectedUser = this.users.find(user => user.id === +selectedUserId);
+        }
+      }
     );
   }
   handleSuccessfulResponse(response) {
     this.users = response;
   }
+
+  viewUser(id: number) { 
+    
+    this.router.navigate(['admin','users'], {queryParams : {id, action: 'view' } });
+  }
+  addUser() {
+    this.selectedUser = new Customer();
+    this.router.navigate(['admin', 'users'], { queryParams: { action: 'add' } });
+  }
+
+  
 
 
 }
